8e2ed2f68b3b30b12e40916b4e7aeca2234f771c
mohibbullah272 <bullahmohi50@gmail.com>
2025-10-12 11:58:21 +0600

    fix some bug

--- dist/app/middlewares/appError.js
+++ dist/app/middlewares/appError.js
@@ -1,9 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class AppError extends Error {
-    statusCode;
-    status;
-    isOperational;
     constructor(message, statusCode) {
         super(message);
         this.statusCode = statusCode;
